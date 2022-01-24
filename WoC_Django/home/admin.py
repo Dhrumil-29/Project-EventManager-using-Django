931cc88825b57121a7d20367da16aa58303327b1
Dhrumil_29 <68535781+Dhrumil-29@users.noreply.github.com>
2022-01-24 19:58:18 +0530

    Add files via upload

---
+++ WoC_Django/home/admin.py
@@ -0,0 +1,7 @@
+from django.contrib import admin
+from home.models import sending
+from home.models import load
+
+# Register your models here.
+admin.site.register(sending)
+admin.site.register(load)
