fd24387e057a79d0c2e6d9163901a718a5ef445b
Dhrumil_29 <68535781+Dhrumil-29@users.noreply.github.com>
2022-01-31 23:18:27 +0530

    Delete WoC_Django directory

--- WoC_Django/home/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from home.models import sending
-from home.models import load
-
-# Register your models here.
-admin.site.register(sending)
-admin.site.register(load)
